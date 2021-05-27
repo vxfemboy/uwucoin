c99b00b818a5d15942e4308e18a9649e029055f1
legitnull <legitnull0@gmail.com>
2021-05-26 20:07:27 -0600

    intro public mods

--- src/blockchain/block.rs
+++ src/blockchain/block.rs
@@ -1,6 +1,13 @@
 use std::fmt::{ self, Debug, Formatter };
+use crate::BlkHash;
 
 pub struct Blk {
+    pub index: u32,
+    pub timestampt: u128,
+    pub hash: BlkHash,
+    pub prev_blk: BlkHash,
+    pub nonce: u64,
+    pub payload: String,
 }
 
 impl Debug for Blk {
@@ -10,4 +17,6 @@ impl Debug for Blk {
 }
 
 impl Blk {
+
+
 }
