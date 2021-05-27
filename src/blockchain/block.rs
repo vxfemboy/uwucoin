95ff4a26276c9412a2da73d03607ecf6d8cedb35
legitnull <legitnull0@gmail.com>
2021-05-26 20:52:53 -0600

    added more hashing of 0 hex;

--- src/blockchain/block.rs
+++ src/blockchain/block.rs
@@ -3,7 +3,7 @@ use crate::BlkHash;
 
 pub struct Blk {
     pub index: u32,
-    pub timestampt: u128,
+    pub timestamp: u128,
     pub hash: BlkHash,
     pub prev_blk: BlkHash,
     pub nonce: u64,
@@ -12,11 +12,25 @@ pub struct Blk {
 
 impl Debug for Blk {
     fn fmt (&self, f: &mut Formatter) -> fmt::Result {
-        write!(f, "blk")
+        write!(f, "blk[{}]: {} at {} w/ {}", &self.index, &hex::encode(&self.hash), &self.timestamp, &self.payload )
     }
 }
 
 impl Blk {
-
-
+    pub fn new (
+        index: u32,
+        timestamp: u128,
+        prev_blk: BlkHash,
+        nonce: u64,
+        payload: String,
+    ) -> Self {
+        Blk {
+            index,
+            timestamp,
+            hash: vec![0; 32], // move away from sha256 eventually
+            prev_blk, 
+            nonce,
+            payload,
+        }
+    }
 }
