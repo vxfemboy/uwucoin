a098889abfdfee5213e7f0c2bf9217133733636a
legitnull <legitnull0@gmail.com>
2021-05-27 19:14:12 -0600

    It fucking compiles \:D/

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,7 @@ fn main() {
     println!("blockchain test\n\n");
     let block = Blk::new(0,0,vec![0, 32], 0, "Gen blk".to_owned());
     println!("{:?}", &block); //change debug to mainstream
-    let uwu = block.hash();
+    let uwuh = block.hash();
 
-    println!("{:?}", &uwu)
+    println!("{:?}", &uwuh)
 }
