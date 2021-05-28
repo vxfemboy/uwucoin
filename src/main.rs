d515c29f90bb5126fca1dff899f3547eff078034
legitnull <legitnull0@gmail.com>
2021-05-28 12:03:31 -0600

    added difficulty bytes

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,7 @@ use crate::blockchain::hash::*;
 
 fn main() {
     println!("blockchain test\n\n");
+
     let mut block = Blk::new(0,0,vec![0, 32], 0, "Gen blk".to_owned());
     println!("{:?}", &block); //change debug to mainstream
     let uwuh = block.hash();
