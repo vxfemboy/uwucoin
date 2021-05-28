2152d33186b3a1c440c6403a1e6e526603ab929d
legitnull <legitnull0@gmail.com>
2021-05-28 12:29:24 -0600

    fixed main

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ use crate::blockchain::hash::*;
 fn main() {
     println!("blockchain test\n\n");
 
-    let mut block = Blk::new(0,0,vec![0, 32], 0, "Gen blk".to_owned());
+    let mut block = Blk::new(0,0,vec![0, 32], 0, "Gen blk".to_owned(), 0x0000ffffffffffffffffffffffffff);
     println!("{:?}", &block); //change debug to mainstream
     let uwuh = block.hash();
 
