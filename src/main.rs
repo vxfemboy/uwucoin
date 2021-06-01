7070979b88ada327290bae8e477423ed3b3eaeef
legitnull <legitnull0@gmail.com>
2021-05-31 22:53:49 -0600

    fin up mine

--- src/main.rs
+++ src/main.rs
@@ -6,9 +6,9 @@ use crate::blockchain::hash::*;
 
 fn main() {
     println!("blockchain test\n\n");
-
+    // create a better and more secure difficulty script
     let mut block = Blk::new(0,0,vec![0, 32], 0, "Gen blk".to_owned(), 0x0000ffffffffffffffffffffffffff);
-    
+    //unmined
     block.hash = block.hash();
     println!("{:?}", &block);
 
